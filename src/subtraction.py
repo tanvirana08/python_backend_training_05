b3cdba0e059c7b2c2302a13bab4af24d98f21615
Tanu Rana <tanu.rana@compunnel.com>
2025-09-02 16:02:19 +0530

    change in subtraction code

--- src/subtraction.py
+++ src/subtraction.py
@@ -4,9 +4,6 @@ q = 5
 
 def subtract(val1, val2):
     diff = val1 - val2
-
-    return diff
-
-
-subtraction = subtract(p, q)
-print("Subtraction_result:", subtraction)
+    return diff 
+subtraction_result = subtract(p, q)
+print("result_subtraction:", subtraction_result)           
